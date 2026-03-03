537549b28ba690470bbbe9341a9fdee9f07039a1
yani5577 <yaniyasundaram@gmail.com>
2026-03-03 21:26:37 +0530

    render oops as banner using array and loops

--- main.java
+++ main.java
@@ -1,46 +1,55 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
+        // Create array to store banner lines
+        String[] banner = new String[7];
+
+        // Populate array using String.join()
+        banner[0] = String.join(" ",
                 "  *****  ",
                 "  *****  ",
                 " ******  ",
-                "  *****  "));
+                "  *****  ");
 
-        System.out.println(String.join(" ",
+        banner[1] = String.join(" ",
                 " *     * ",
                 " *     * ",
                 " *     * ",
-                " *     * "));
+                " *     * ");
 
-        System.out.println(String.join(" ",
+        banner[2] = String.join(" ",
                 " *     * ",
                 " *     * ",
                 " *     * ",
-                " *       "));
+                " *       ");
 
-        System.out.println(String.join(" ",
+        banner[3] = String.join(" ",
                 " *     * ",
                 " *     * ",
                 " ******  ",
-                "  *****  "));
+                "  *****  ");
 
-        System.out.println(String.join(" ",
+        banner[4] = String.join(" ",
                 " *     * ",
                 " *     * ",
                 " *       ",
-                "       * "));
+                "       * ");
 
-        System.out.println(String.join(" ",
+        banner[5] = String.join(" ",
                 " *     * ",
                 " *     * ",
                 " *       ",
-                " *     * "));
+                " *     * ");
 
-        System.out.println(String.join(" ",
+        banner[6] = String.join(" ",
                 "  *****  ",
                 "  *****  ",
                 " *       ",
-                "  *****  "));
+                "  *****  ");
+
+        // Use enhanced for loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
