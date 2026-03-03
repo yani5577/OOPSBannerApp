dc262fc8b0a4527bdd135cd1696bbb11164827d5
yani5577 <yaniyasundaram@gmail.com>
2026-03-03 21:20:51 +0530

    render oops as banner using string join

--- main.java
+++ main.java
@@ -1,25 +1,46 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Line 1
-        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " ******  ",
+                "  *****  "));
 
-        // Line 2
-        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * "));
 
-        // Line 3
-        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *       "));
 
-        // Line 4
-        System.out.println(" *     * " + " *     * " + " ******  " + "  *****  ");
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                "  *****  "));
 
-        // Line 5
-        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                "       * "));
 
-        // Line 6
-        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
+        System.out.println(String.join(" ",
+                " *     * ",
+                " *     * ",
+                " *       ",
+                " *     * "));
 
-        // Line 7
-        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                " *       ",
+                "  *****  "));
     }
 }
